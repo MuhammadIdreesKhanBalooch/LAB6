0d132e47e121c9931a927c55aa9c8ff3e55f57a1
MuhammadIdreesKhanBalooch <108642061+MuhammadIdreesKhanBalooch@users.noreply.github.com>
2022-08-18 06:06:05 +0500

    Update test.java

--- test.java
+++ test.java
@@ -17,9 +17,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
-public class NewTest1 {
+public class test {
 
 	WebDriver driver;
 	
@@ -29,7 +30,7 @@ public class NewTest1 {
 	  public void beforeTest() {
 		  
 		// setting selenium
-				System.setProperty("webdriver.chrome.driver", "C:\\Users\\4073\\Desktop\\chromedr\\chromedriver.exe");
+				System.setProperty("webdriver.chrome.driver", "C:\\Users\\khan\\chrome driver\\chromedriver.exe");
 				driver = new ChromeDriver();
 				driver.manage().window().maximize();
 		// hitting url
@@ -47,9 +48,13 @@ public class NewTest1 {
 	  	text2.sendKeys(Keys.CONTROL + "a");
 	  	text2.sendKeys( Keys.BACK_SPACE);
 	  	text2.sendKeys("A supreme lover of plants is automating the browser!");
-	 
+	//assertion for textfield enbale checkup 	
+	  		Assert.assertTrue(driver.findElement(By.xpath("//div[contains(@class,'navbar')]//following :: textarea[contains(text(),'The ')]")).isEnabled()); 
+	  
+		  	System.out.println("textarea functionality is " + driver.findElement(By.xpath("//div[contains(@class,'navbar')]//following :: textarea[contains(text(),'The ')]")).isEnabled());
 	  	
 	  	
+
   }
   
   @Test(priority=1)
@@ -66,8 +71,9 @@ public class NewTest1 {
 	  	driver.findElement(By.xpath("// form[contains(@name,'form')]//child::input[contains(@type,'password')]")).sendKeys("8932");
 	//login
 	  	driver.findElement(By.xpath("// form[contains(@name,'form')]//child::button[contains(@type,'button')]")).click();
-	 
-  
+	//assert for login button enability
+	  	Assert.assertTrue(driver.findElement(By.xpath("// form[contains(@name,'form')]//child::button[contains(@type,'button')]")).isEnabled());
+	  	System.out.println("login button functionality is " +  driver.findElement(By.xpath("// form[contains(@name,'form')]//child::button[contains(@type,'button')]")).isEnabled());
   }
   
   @Test(priority=2)
@@ -80,8 +86,9 @@ public class NewTest1 {
 		WebElement frame2 = driver.findElement(By.xpath("(//div[contains(@class,'widget-content')]//descendant :: iframe)[2]"));
 		driver.switchTo().frame(frame2);
 		driver.switchTo().defaultContent();
-	  
-	  
+	 //assertion for frame switching 
+	  Assert.assertTrue(driver.findElement(By.xpath("(//div[contains(@class,'widget-content')]//descendant :: iframe)[2]")).isDisplayed());
+	  System.out.println("switching to frame2 is: " + driver.findElement(By.xpath("(//div[contains(@class,'widget-content')]//descendant :: iframe)[2]")).isDisplayed());
   }
   
   @Test(priority=3)
@@ -94,15 +101,20 @@ public class NewTest1 {
 	  	driver.findElement(By.xpath("// form[contains(@name,'login')]//child::input[contains(@type,'button')]")).click();  
 	//alert
 	  	driver.switchTo().alert().accept();
+	  	
   }
   
   @Test(priority=4)
   public void hyundai()	{
-	  	driver.findElement(By.xpath("//div[contains(@class,'widget HTML')]//child :: option[contains(text(),'Hyundai')]")).click();
+	  WebElement hyundai = driver.findElement(By.xpath("//div[contains(@class,'widget HTML')]//child :: option[contains(text(),'Hyundai')]"));
+	  hyundai.click();
 	//dropdown
 		driver.findElement(By.xpath("//div[contains(@class,'widget HTML')]//child :: select[contains(@class,'combo')]")).click();
 	//doc3
 		driver.findElement(By.xpath("//select[contains(@class,'combo')]//child :: option[contains(@value,'jkl')]"));			
+	//assert for "Hyundai" spell check
+	  	Assert.assertEquals("Hyundai", hyundai.getText());
+	  	System.out.println("hyundai spell check passed" + hyundai.getText());	
   }
   
   @Test(priority=5)
@@ -119,13 +131,18 @@ public class NewTest1 {
 			btn2.click();
 	//checking enability
 			System.out.println("button 2 is" + btn2.isEnabled());
+			
   }
   
   @Test(priority=6)
   public void threeButtons() {
+	 //submit + login + register button
 	  driver.findElement(By.xpath("//div[contains(@class,'widget-content')]//child :: button[contains(text(),\"Submit\")]")).click();
 	  driver.findElement(By.xpath("//div[contains(@class,'widget-content')]//child :: button[contains(text(),\"Login\")]")).click();
 	  driver.findElement(By.xpath("//div[contains(@class,'widget-content')]//child :: button[contains(text(),\"Register\")]")).click();
+	 //assert for register button functionality 
+	  Assert.assertTrue(driver.findElement(By.xpath("//div[contains(@class,'widget-content')]//child :: button[contains(text(),\"Register\")]")).isEnabled());
+	  System.out.println("register button clickable " + driver.findElement(By.xpath("//div[contains(@class,'widget-content')]//child :: button[contains(text(),\"Register\")]")).isEnabled() );
   }
   
   @Test(priority=7)
@@ -135,6 +152,7 @@ public class NewTest1 {
 		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[contains(@id,'delete')]")));
 		driver.findElement(By.xpath("//div[contains(@class,'widget-content')]//child :: input[contains(@value,'ClickAfter')]")).click();
 		driver.switchTo().alert().accept();
+
   } 
   
   @Test(priority=8)
@@ -160,13 +178,14 @@ public void newPopWindow() {
 			System.out.println(para1.getText() + "\n" + para2.getText());
 	//switch to parent window	
 			driver.switchTo().window(parentWindowHandler);
-	  
+	Assert.assertEquals(driver.findElement(By.className("sub")).getText(), "a paragraph");
+	
   } 
   
   @Test(priority=9)
   public void uploadFile() {
 	  //upload file
-	  		driver.findElement(By.xpath("//form[contains(@action,'demo_form')]//child :: input")).sendKeys("C:\\Users\\4073\\Desktop\\companyHiring.PNG");
+	  		driver.findElement(By.xpath("//form[contains(@action,'demo_form')]//child :: input")).sendKeys("C:\\Users\\khan\\Desktop\\train.PNG");
 	// my btn functionality before clicking "try it"
 			WebElement myBtn = driver.findElement(By.xpath("//button[contains(text(),'My')]"));
 			System.out.println("'My Button' functionality before clicking 'Try It' button" + myBtn.isEnabled());
@@ -177,6 +196,7 @@ public void newPopWindow() {
 			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'My')]")));
 			System.out.println("'My Button' functionality after clicking 'Try It' button" + myBtn.isEnabled());
+			
   }
   
   @Test(priority=10)
@@ -200,9 +220,11 @@ public void newPopWindow() {
   public void gender() {
 	 //gender radio 
 	  		driver.findElement(By.xpath("(//div[contains(@class,'widget HTML')]//descendant :: input)[15]")).click();
-	 //clickToget alert
+	 //clickToget alert	
 	  		driver.findElement(By.xpath("//div[contains(@class,'widget-content')]//descendant :: input [contains(@value,'ClickTo')]")).click();
-	 //handle alert
+ 	//handle alert
+	  		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+	  		wait.until(ExpectedConditions.alertIsPresent());
 	  		driver.switchTo().alert().accept();
 	 //select blue
 	  		driver.findElement(By.xpath("//input[contains(@value,'blue')]")).click();
@@ -216,7 +238,6 @@ public void newPopWindow() {
 	  		
 	  		WebElement getPrompt = driver.findElement(By.xpath("//input[contains(@value,'GetPrompt')]"));
 			getPrompt.click();
-			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 			Alert promptPresent = wait.until(ExpectedConditions.alertIsPresent());
 			System.out.println(promptPresent.getText());
 	//put name in alert
@@ -247,7 +268,7 @@ public void newPopWindow() {
 			driver.findElement(By.xpath("//input[contains(@value,'Book')]")).click();
 	//deselect book
 			driver.findElement(By.xpath("//input[contains(@value,'Book')]")).click();	
-	//deropdown
+	//drop down button
 			driver.findElement(By.xpath("//button[contains(@class,'drop')]")).click();
 			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Facebook')]")));
